3847522bfcde8f0189c425cdc92befa361f43fbb
Lauren Doss <doss.laurene@gmail.com>
2020-03-27 09:51:01 -0700

    added functions for each role. once a user selects a particular role, they will be prompted with questions specific to that role

--- app.js
+++ app.js
@@ -1,35 +1,118 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+// const Manager = require("./lib/Manager");
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
-const render = require("./lib/htmlRenderer");
-​
-​
+// const path = require("path");
+// const fs = require("fs");
+// const OUTPUT_DIR = path.resolve(__dirname, "output")
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
+// ​
+// const render = require("./lib/htmlRenderer");
+
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-​
+
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-​
+
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-​
+
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an 
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work!```
+function promptUserManager() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is your Office Number?"
+        },
+
+    ]).then(answers => {
+        console.log(answers);
+    });
+}
+
+function promptUserEngineer() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "GitHub",
+            message: "What is your GitHub username?"
+        },
+    ]).then(answers => {
+        console.log(answers);
+    });
+}
+function promptUserIntern() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "school",
+            message: "What School do you attend?"
+        },
+    ]).then(answers => {
+        console.log(answers);
+    })
+}
+function promptUserGeneral() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your Employee ID Number?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email address?"
+        },
+        {
+            type: "list",
+            name: "employeeType",
+            message: "Please select your role from the list below: ",
+            choices: [
+                "Manager",
+                "Engineer",
+                "Intern"
+            ]
+        },
+
+    ]).then(answers => {
+        console.log(answers);
+
+        if (answers.employeeType === "Manager") {
+            promptUserManager(); 
+
+    } else if (answers.employeeType === "Engineer") {
+
+        promptUserEngineer();
+    } else if (answers.employeeType === "Intern") {
+
+        promptUserIntern();
+    }
+})
+
+}
+promptUserGeneral();
+
+
+
+//engineer
